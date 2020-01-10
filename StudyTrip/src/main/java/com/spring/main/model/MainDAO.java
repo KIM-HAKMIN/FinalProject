8893a361c208d32660aaebcc1468c9e1f9f53721
user1 <user1@DESKTOP-8UC660Q>
2020-01-10 18:09:23 +0900

    이민형 2020-01-10 18:09 헤더,푸터 초기설정

---
+++ StudyTrip/src/main/java/com/spring/main/model/MainDAO.java
@@ -0,0 +1,23 @@
+package com.spring.main.model;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MainDAO implements InterMainDAO {
+
+	@Autowired   // Type에 따라 알아서 Bean 을 주입해준다.
+	private SqlSessionTemplate sqlsession;
+	
+	
+	
+	@Override
+	public String getToday() {
+
+		String today = sqlsession.selectOne("main.getToday");
+		
+		return today;
+	}
+
+}
